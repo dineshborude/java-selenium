add7bbd76cf8c5f1a0328996377aeae62c6c6ec7
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-01-09 14:33:14 +0530

    Java Basics : Class/Objects

---
+++ src/Day2.java
@@ -0,0 +1,21 @@
+class Day2 {
+    //Start of body
+
+// Java Class : Template or blueprint which describes state/behavior of its object.
+// Class is used to create Objects.
+// Objects : Objects are nothing but instance of class.
+// In Order to execute any code in Java, we need main method :
+
+    public static void main(String[] args) {
+
+        System.out.println("Object : Instance of class ");
+        //In java, An Object is created when somaone says "new" keyword
+        //Each "new" word creates a new object of class.
+
+        //JDK : JRE(JVM+Lib Files) + JAVA Compiler
+
+        System.out.println("Garbage Collection : It destroys the objects which are not in use");
+    }
+
+
+}
